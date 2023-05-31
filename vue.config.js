ebfc142f260e4342c743a7f4fc13bb7847ce5cae
weineko <weineko@glxycl.top>
2023-06-01 03:13:21 +0800

    修改配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 跳过检查host
+  devServer: { disableHostCheck: true }
 })
