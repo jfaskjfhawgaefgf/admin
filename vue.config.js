eef569ddcf9f96bd0eccfb97948632df9639c65c
weineko <weineko@glxycl.top>
2023-06-01 15:06:02 +0800

    修改.gitigonre

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
+const weburl = process.env.VUE_APP_API_URL
 module.exports = defineConfig({
   transpileDependencies: true,
-  // 跳过检查host
   devServer: {
     allowedHosts: [
-      'demo.filmpie.glxycl.top'  
+      weburl
     ],
-  }
+  },
+  publicPath: './'
 })
